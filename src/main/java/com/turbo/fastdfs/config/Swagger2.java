7dca286917d314705ad7fbda3498c3f5177fbd17
lingc <2996547275@qq.com>
2019-04-15 17:10:22 +0800

    feat:增加swagger

--- src/main/java/com/turbo/fastdfs/config/Swagger2.java
+++ src/main/java/com/turbo/fastdfs/config/Swagger2.java
@@ -1,10 +1,13 @@
 package com.turbo.fastdfs.config;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @ClassName Swagger2
  * @Author lingc
  * @Date 2019/4/15
  * @Description
  **/
+@EnableEurekaClient
 public class Swagger2 {
 }
